4f7aa7f274d86c069a440dfabcfe47d6c51fc2b4
Dkolomy <Dkolomy@hotmail.com>
2026-04-21 22:36:33 -0400

    Refactor usersSlice to utilize createEntityAdapter for improved user state management
    - Integrated createEntityAdapter for managing user state, enhancing performance and clarity.
    - Updated initialState to use the adapter's getInitialState method.
    - Modified fetchUsers fulfilled case to utilize the adapter for setting user data.
    - Added selectors for retrieving users using the entity adapter.

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,4 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { 
+  createSlice, 
+  createEntityAdapter,
+} from '@reduxjs/toolkit'
 
 import { client } from '../../api/client'
 import { createAppAsyncThunk } from '../../app/withTypes'
@@ -12,41 +15,37 @@ type User = {
   name: string
 }
 
+const usersAdapter = createEntityAdapter<User>()
+
+const initialState = usersAdapter.getInitialState()
+
 export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
   const response = await client.get<User[]>('/fakeApi/users')
   return response.data
 })
-
-const initialState: User[] = []
-
-// const initialState: User[] = [
-//   { id: '0', name: 'Tianna Jenkins' },
-//   { id: '1', name: 'Kevin Grant' },
-//   { id: '2', name: 'Madison Price' },
-// ]
 
 const usersSlice = createSlice({  
   name: 'users',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchUsers.fulfilled, (_state, action) => {
-      return action.payload
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      usersAdapter.setAll(state, action.payload)
     })
   }
 })
 
 export default usersSlice.reducer
 
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+} = usersAdapter.getSelectors((state: RootState) => state.users)
+
 export const selectCurrentUser = (state: RootState) => {
-  console.log('selectCurrentUser state', state)
   const currentUsername = selectCurrentUsername(state)
-  console.log('selectCurrentUser currentUsername', currentUsername)
-  return selectUserById(state, currentUsername ?? undefined)
-}
-
-export const selectAllUsers = (state: RootState) => state.users
-
-export const selectUserById = (state: RootState, userId: string | undefined) => {
-  return state.users.find((user) => user.id === userId)
+  if (!currentUsername) {
+    return
+  }
+  return selectUserById(state, currentUsername)
 }
